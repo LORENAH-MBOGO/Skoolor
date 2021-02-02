9a2de29e86363f3d318893d156f6e042ef643ce8
Lorenah <lorayne911@gmail.com>
2021-02-02 20:40:34 +0300

    create student class

--- app/src/main/java/com/skylar/skoolor/Students.java
+++ app/src/main/java/com/skylar/skoolor/Students.java
@@ -1,4 +1,59 @@
 package com.skylar.skoolor;
 
 public class Students {
+    private    String StPassword;
+    private String StEmail;
+    private String StPhone;
+    private  String StUsername;
+    private  String StRoll;
+    Students(){ }
+
+    public Students( String stUsername,String stPassword, String stEmail, String stPhone,String stRoll) {
+        this.StPassword = stPassword;
+        this.StEmail = stEmail;
+        this.StUsername=stUsername;
+        this.StPhone = stPhone;
+        this.StRoll=stRoll;
+    }
+
+    public void setStUsername(String stUsername) {
+        StUsername = stUsername;
+    }
+
+    public void setStRoll(String stRoll) {
+        StRoll = stRoll;
+    }
+
+    public String getStRoll() {
+        return StRoll;
+    }
+
+    public String getStUsername() {
+        return StUsername;
+    }
+
+    public void setStPassword(String stPassword) {
+        this.StPassword = stPassword;
+    }
+
+    public void setStEmail(String stEmail) {
+        this.StEmail = stEmail;
+    }
+
+    public void setStPhone(String stPhone) {
+        this.StPhone = stPhone;
+    }
+
+    public String getStPassword() {
+        return this.StPassword;
+    }
+
+    public String getStEmail() {
+        return this.StEmail;
+    }
+
+    public String getStPhone() {
+        return this.StPhone;
+    }
+}
 }
